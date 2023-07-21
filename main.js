74ccca199d314c267ac648f327448d7c8fe3acb4
watti1016 <115801130+watti1016@users.noreply.github.com>
2023-07-21 10:49:51 +0900

    main.js

--- main.js
+++ main.js
@@ -1,47 +1,30 @@
-// スライド　大画面
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-// スライド選択画面
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  autoplay: true,
-  centerMode: true,
-  focusOnSelect: true
-});
+
 // ruleコンテンツ要素手前で判定
-var sFlag = 0;
-var flg1 = 0;
-var flg2 = 0;
-var flg3 = 0;
-var flg4 = 0;
-var flg5 = 0;
+let sFlag = 0;
+let flg1 = 0;
+let flg2 = 0;
+let flg3 = 0;
+let flg4 = 0;
+let flg5 = 0;
 
 
 $(window).scroll(function(){
-  var height = $(this).scrollTop();
-  var countS = $(".skill_title").length;
+  let height = $(this).scrollTop();
+  let countS = $(".skill_title").length;
 
   for(var i = 0;i < countS; i++){
-    var target = i;
-    var heightS = document.querySelectorAll(".skill_title")[target].offsetTop-$(window).height()+300;
+    let target = i;
+    let heightS = document.querySelectorAll(".skill_title")[target].offsetTop-$(window).height()+300;
     //     各ruleコンテンツの位置計算
-    var heightR =
+    let heightR =
         document.querySelectorAll(".rule1")[target].offsetTop-$(window).height()-100;
-    var heightF =
+    let heightF =
         document.querySelectorAll(".rule2")[target].offsetTop-$(window).height()-100;
-    var heightC =
+    let heightC =
         document.querySelectorAll(".rule3")[target].offsetTop-$(window).height()-100;
-    var heightU =
+    let heightU =
         document.querySelectorAll(".rule4")[target].offsetTop-$(window).height()-100;
-    var heightG =
+    let heightG =
         document.querySelectorAll(".rule5")[target].offsetTop-$(window).height()-100;
     if(height > heightS){
       if(sFlag == 0){
@@ -95,7 +78,7 @@ $(document).ready(function(){
 // ======== header表示非表示
 
 $(function(){
-  var pos = 0;
+  let pos = 0;
   $(window).on('scroll',function(){
     if($(this).scrollTop() < pos){
       $('#header .header_top').css('height','100px');
